9714fb5ce90f9cb0985d0615b48451970eb6a0bc
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-04-04 22:02:56 -0600

    Set model_dest to models/binary/convolutional_context_noise

--- bin/train-binary-convolutional-context-noise.sh
+++ bin/train-binary-convolutional-context-noise.sh
@@ -8,7 +8,7 @@ do
     for max_edits in 1 2 3
     do
         edit_cfg="min_edits=1 max_edits=$max_edits"
-        model_dest="models/binary/convolutional_context_xval/"$(echo $editor_cfg $edit_cfg | sed 's, ,_,g')
+        model_dest="models/binary/convolutional_context_noise/"$(echo $editor_cfg $edit_cfg | sed 's, ,_,g')
         model_cfg="$train_cfg $optimizer_cfg $editor_cfg $edit_cfg"
         echo bin/train.py models/binary --mode persistent-background --model-dest $model_dest --model-cfg $model_cfg
     done
