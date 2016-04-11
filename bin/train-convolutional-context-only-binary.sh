1710d1a674d07b3499651972bbd44eeabc2a7308
Nicholas Dronen <nicholas.dronen@pearson.com>
2016-04-11 07:37:29 -0600

    Snapshot of binary convolutional context-only script

--- bin/train-convolutional-context-only-binary.sh
+++ bin/train-convolutional-context-only-binary.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# TODO: what is the effect of varying the size of the context window?
-# TODO: what is the effect of use_real_word_embedding=true?
-
 # This script has the command to train a good context-only (i.e. lexical
-# context of non-word and a candidate) binary model.
+# context of non-word and a candidate) binary model using only a word
+# embedding layer and a deep residual network.  The data are words of
+# length 3.  The training examples are windows of width 5 from sentences
+# taken from Wikipedia; the word at the center of the window a word of
+# length 3 that occurs in the Aspell English dictionary.
+#
+# A model trained with this script achieved F1 of 0.6633 on the validation set.
 
-#n_train=1659738
-n_train=100000
-#model_dest=models/binary/contextual_01
-context_input_width=9
-context_filter_width=9
-use_real_word_embedding=false
-n_context_embed_dims=10
-n_context_filters=2000
+n_train=60000
+n_valid=4500
+model_dest=models/binary/convolutional_context_deep_residual
+vectors_path=data/vectors.pkl.gz
+pickle_path=data/contexts-100-min-100-max-per-word-length-3.pkl
+l2_penalty=0.000001
+n_hidden=10
+n_residual_blocks=10
+n_context_embed_dims=20
 
-    #--model-dest $model_dest \
 bin/train.py models/binary \
-    --mode transient \
-    --model-cfg fully_connected='[1000]' n_residual_blocks=5 n_hidden_residual=1000 n_epoch=500 patience=50 samples_per_epoch=$n_train pickle_path=data/contexts-100-min-100-max-per-word.pkl use_context_model=true use_char_model=false use_real_word_embedding=$use_real_word_embedding batch_normalization=true class_weight_exponent=1 vectors_path=data/vectors.pkl.gz batch_size=128 char_inputs_to_dense_block='[]' char_merge_gaussian_noise_sd=0.0 n_val_samples=1000 optimizer=Adam softmax_max_norm=1 l2_penalty=0.00001 n_context_embed_dims=$n_context_embed_dims n_context_filters=$n_context_filters context_input_width=$context_input_width context_filter_width=$context_filter_width
+    --mode persistent-background \
+    --model-dest $model_dest \
+    --model-cfg fully_connected="[$n_hidden]" n_residual_blocks=$n_residual_blocks n_hidden_residual=$n_hidden n_epoch=500 patience=50 samples_per_epoch=$n_train pickle_path=$pickle_path use_context_model=true context_model_type="convolutional" use_char_model=false batch_normalization=true class_weight_exponent=1 vectors_path=$vectors_path batch_size=100 n_val_samples=$n_valid optimizer=Adam n_context_embed_dims=$n_context_embed_dims train_size=$n_train validation_size=$n_valid l2_penalty=$l2_penalty dropout_residual_p=0.1 n_context_filters=100
